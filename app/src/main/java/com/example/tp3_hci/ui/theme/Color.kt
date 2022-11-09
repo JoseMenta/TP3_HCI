a5d3d6fef04271a8b634026dc202d7788d5c4031
Axel <axel.preitit@gmail.com>
2022-11-09 14:18:59 -0300

    Se definieron los estilos generales de la aplicacion
    Colores, fuente Roboto y tamaños de redondeo

--- app/src/main/java/com/example/tp3_hci/ui/theme/Color.kt
+++ app/src/main/java/com/example/tp3_hci/ui/theme/Color.kt
@@ -2,7 +2,15 @@ package com.example.tp3_hci.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val FitiDarkBlue = Color(0xFF142850)
+val FitiBlue = Color(0xFF27496D)
+val FitiBlueFill = Color(0xFF0F1B33)
+val FitiBlueText = Color(0xFF0F1B33)
+val FitiGrey = Color(0xFFDAE1E7)
+val FitiGreyImage = Color(0xFFE8F1F6)
+val FitiGreyInputText = Color(0xFF808080)
+val FitiGreyButton = Color(0xFFD9D9D9)
+val FitiWhiteText = Color(0xFFF5FAFF)
+val FitiRedError = Color(0xFFFF0000)
+val FitiGreenButton = Color(0xFF00909E)
+val FitiBlackText = Color(0xFF1C1B1F)
--- app/src/main/java/com/example/tp3_hci/ui/theme/Type.kt
+++ app/src/main/java/com/example/tp3_hci/ui/theme/Type.kt
@@ -1,19 +1,37 @@
 package com.example.tp3_hci.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.tp3_hci.R
+
+val RobotoFontFamily = FontFamily(
+    Font(R.font.roboto_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.roboto_black_italic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.roboto_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.roboto_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.roboto_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.roboto_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.roboto_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.roboto_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.roboto_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.roboto_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.roboto_thin, FontWeight.Thin, FontStyle.Normal),
+    Font(R.font.roboto_thin_italic, FontWeight.Thin, FontStyle.Italic)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    defaultFontFamily = RobotoFontFamily
+
+    /* Other default text styles to override
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
+    ),
     button = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W500,
