c0b78f92913ddb011ce6a2fb166474352d3fc980
JoseMenta <jrmenta2@gmail.com>
2022-11-17 12:47:23 -0300

    cambios en la vista de review de rutina y en login para aceptar el enter

--- app/src/main/java/com/example/tp3_hci/data/ui_state/RatingUiState.kt
+++ app/src/main/java/com/example/tp3_hci/data/ui_state/RatingUiState.kt
@@ -1,9 +1,12 @@
 package com.example.tp3_hci.data.ui_state
 
+import com.example.tp3_hci.data.model.RoutineOverview
+
 data class RatingUiState(
     val isFetching: Boolean = false,
     val message: String? = null,
-    val rating: Int = 0
+    val rating: Int = 0,
+    val routine: RoutineOverview? = null
 ){
 
 }
